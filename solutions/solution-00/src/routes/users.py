b2c8f042609254d50b62569b52328b04bb35bd43
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-26 09:36:52 +0200

    protected route for jwt

--- solutions/solution-00/src/routes/users.py
+++ solutions/solution-00/src/routes/users.py
@@ -2,7 +2,7 @@
 This module contains the routes for the users endpoints.
 """
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from src.controllers.users import (
     create_user,
     delete_user,
@@ -10,6 +10,9 @@ from src.controllers.users import (
     get_users,
     update_user,
 )
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from src.models import User
+from src import bcrypt
 
 users_bp = Blueprint("users", __name__, url_prefix="/users")
 
@@ -19,3 +22,20 @@ users_bp.route("/", methods=["POST"])(create_user)
 users_bp.route("/<user_id>", methods=["GET"])(get_user_by_id)
 users_bp.route("/<user_id>", methods=["PUT"])(update_user)
 users_bp.route("/<user_id>", methods=["DELETE"])(delete_user)
+
+@users_bp.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.password_hash, password):
+        access_token = create_access_token(identity={'email': user.email, 'is_admin': user.is_admin})
+        return jsonify(access_token=access_token), 200
+    return jsonify({"msg": "Wrong email or password"}), 401
+
+# Example of a protected route
+@users_bp.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
