305f78648aa5bb70903527c4c7c8a6940ccde90c
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-27 09:50:31 +0200

    table place amenity

---
+++ sql/create_tables/create_place_amenity.sql
@@ -0,0 +1,9 @@
+USE hbnb;
+
+CREATE TABLE PlaceAmenity (
+    place_id VARCHAR(36),
+    amenity_id VARCHAR(36),
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES Place(id),
+    FOREIGN KEY (amenity_id) REFERENCES Amenity(id)
+);
