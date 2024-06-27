48733f0fac114af72a157da326adb4ddb33e99f3
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-27 09:48:32 +0200

    table city

---
+++ sql/create_tables/create_city.sql
@@ -0,0 +1,8 @@
+USE hbnb;
+
+CREATE TABLE City (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    country_code VARCHAR(2),
+    FOREIGN KEY (country_code) REFERENCES Country(code)
+);
