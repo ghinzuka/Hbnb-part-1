7e22b2e90b69b344179e3d78553ba86f6bd23eb6
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-27 09:49:01 +0200

    table place

---
+++ sql/create_tables/create_place.sql
@@ -0,0 +1,18 @@
+USE hbnb;
+
+CREATE TABLE Place (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    description TEXT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    host_id VARCHAR(36),
+    city_id VARCHAR(36),
+    price_per_night INT NOT NULL,
+    number_of_rooms INT NOT NULL,
+    number_of_bathrooms INT NOT NULL,
+    max_guests INT NOT NULL,
+    FOREIGN KEY (host_id) REFERENCES User(id),
+    FOREIGN KEY (city_id) REFERENCES City(id)
+);
